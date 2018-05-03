440f99f9eb3ee0423330d1284c157de8210438dd
grantmacken <grantmacken@gmail.com>
2018-05-04 06:19:16 +1200

    make a start

--- commonmark.lua
+++ commonmark.lua
@@ -6,92 +6,160 @@ local c = ffi.load("libcmark")
 
 ffi.cdef[[
 
-      typedef enum {
-         /* Block */
-         CMARK_NODE_DOCUMENT,
-         CMARK_NODE_BLOCK_QUOTE,
-         CMARK_NODE_LIST,
-         CMARK_NODE_LIST_ITEM,
-         CMARK_NODE_CODE_BLOCK,
-         CMARK_NODE_HTML,
-         CMARK_NODE_PARAGRAPH,
-         CMARK_NODE_HEADER,
-         CMARK_NODE_HRULE,
-         CMARK_NODE_REFERENCE_DEF,
+  /* ## Simple Interface */
+  char *cmark_markdown_to_html(const char *text, size_t len, int options);
 
-         CMARK_NODE_FIRST_BLOCK = CMARK_NODE_DOCUMENT,
-         CMARK_NODE_LAST_BLOCK  = CMARK_NODE_REFERENCE_DEF,
+ /* ## Node Structure */
 
-         /* Inline */
-         CMARK_NODE_TEXT,
-         CMARK_NODE_SOFTBREAK,
-         CMARK_NODE_LINEBREAK,
-         CMARK_NODE_INLINE_CODE,
-         CMARK_NODE_INLINE_HTML,
-         CMARK_NODE_EMPH,
-         CMARK_NODE_STRONG,
-         CMARK_NODE_LINK,
-         CMARK_NODE_IMAGE,
+  typedef enum {
+  /* Error status */
+  CMARK_NODE_NONE,
 
-         CMARK_NODE_FIRST_INLINE = CMARK_NODE_TEXT,
-         CMARK_NODE_LAST_INLINE  = CMARK_NODE_IMAGE,
-                   } cmark_node_type;
+    /* Block */
+    CMARK_NODE_DOCUMENT,
+    CMARK_NODE_BLOCK_QUOTE,
+    CMARK_NODE_LIST,
+    CMARK_NODE_ITEM,
+    CMARK_NODE_CODE_BLOCK,
+    CMARK_NODE_HTML_BLOCK,
+    CMARK_NODE_CUSTOM_BLOCK,
+    CMARK_NODE_PARAGRAPH,
+    CMARK_NODE_HEADING,
+    CMARK_NODE_THEMATIC_BREAK,
 
-      typedef enum {
-         CMARK_NO_LIST,
-         CMARK_BULLET_LIST,
-         CMARK_ORDERED_LIST
-                   }  cmark_list_type;
+    CMARK_NODE_FIRST_BLOCK = CMARK_NODE_DOCUMENT,
+    CMARK_NODE_LAST_BLOCK = CMARK_NODE_THEMATIC_BREAK,
 
-      typedef enum {
-         CMARK_PERIOD_DELIM,
-         CMARK_PAREN_DELIM
-                   } cmark_delim_type;
+    /* Inline */
+    CMARK_NODE_TEXT,
+    CMARK_NODE_SOFTBREAK,
+    CMARK_NODE_LINEBREAK,
+    CMARK_NODE_CODE,
+    CMARK_NODE_HTML_INLINE,
+    CMARK_NODE_CUSTOM_INLINE,
+    CMARK_NODE_EMPH,
+    CMARK_NODE_STRONG,
+    CMARK_NODE_LINK,
+    CMARK_NODE_IMAGE,
 
-      typedef struct cmark_node cmark_node;
-      typedef struct cmark_parser cmark_parser;
+    CMARK_NODE_FIRST_INLINE = CMARK_NODE_TEXT,
+    CMARK_NODE_LAST_INLINE = CMARK_NODE_IMAGE,
+  } cmark_node_type;
 
-      char *cmark_markdown_to_html(const char *text, int len);
-      cmark_node* cmark_node_new(cmark_node_type type);
-      void cmark_node_free(cmark_node *node);
-      cmark_node* cmark_node_next(cmark_node *node);
-      cmark_node* cmark_node_previous(cmark_node *node);
-      cmark_node* cmark_node_parent(cmark_node *node);
-      cmark_node* cmark_node_first_child(cmark_node *node);
-      cmark_node* cmark_node_last_child(cmark_node *node);
-      cmark_node_type cmark_node_get_type(cmark_node *node);
-      const char* cmark_node_get_string_content(cmark_node *node);
-      int cmark_node_set_string_content(cmark_node *node, const char *content);
-      int cmark_node_get_header_level(cmark_node *node);
-      int cmark_node_set_header_level(cmark_node *node, int level);
-      cmark_list_type cmark_node_get_list_type(cmark_node *node);
-      int cmark_node_set_list_type(cmark_node *node, cmark_list_type type);
-      int cmark_node_get_list_start(cmark_node *node);
-      int cmark_node_set_list_start(cmark_node *node, int start);
-      int cmark_node_get_list_tight(cmark_node *node);
-      int cmark_node_set_list_tight(cmark_node *node, int tight);
-      const char* cmark_node_get_fence_info(cmark_node *node);
-      int cmark_node_set_fence_info(cmark_node *node, const char *info);
-      const char* cmark_node_get_url(cmark_node *node);
-      int cmark_node_set_url(cmark_node *node, const char *url);
-      const char* cmark_node_get_title(cmark_node *node);
-      int cmark_node_set_title(cmark_node *node, const char *title);
-      int cmark_node_get_start_line(cmark_node *node);
-      int cmark_node_get_start_column(cmark_node *node);
-      int cmark_node_get_end_line(cmark_node *node);
-      void cmark_node_unlink(cmark_node *node);
-      int cmark_node_insert_before(cmark_node *node, cmark_node *sibling);
-      int cmark_node_insert_after(cmark_node *node, cmark_node *sibling);
-      int cmark_node_prepend_child(cmark_node *node, cmark_node *child);
-      int cmark_node_append_child(cmark_node *node, cmark_node *child);
-      cmark_parser *cmark_parser_new();
-      void cmark_parser_free(cmark_parser *parser);
-      cmark_node *cmark_parser_finish(cmark_parser *parser);
-      void cmark_parser_feed(cmark_parser *parser, const char *buffer, size_t len);
-      cmark_node *cmark_parse_document(const char *buffer, size_t len);
-      char *cmark_render_ast(cmark_node *root);
-      char *cmark_render_html(cmark_node *root);
-]]
+  typedef enum {
+    CMARK_NO_LIST,
+    CMARK_BULLET_LIST,
+    CMARK_ORDERED_LIST
+  } cmark_list_type;
+
+  typedef enum {
+    CMARK_NO_DELIM,
+    CMARK_PERIOD_DELIM,
+    CMARK_PAREN_DELIM
+  } cmark_delim_type;
+
+  typedef struct cmark_node cmark_node;
+  typedef struct cmark_parser cmark_parser;
+  typedef struct cmark_iter cmark_iter;
+  typedef struct cmark_mem {
+    void *(*calloc)(size_t, size_t);
+    void *(*realloc)(void *, size_t);
+    void (*free)(void *);
+  } cmark_mem;
+
+  /* ## Creating and Destroying Nodes */
+  cmark_node *cmark_node_new(cmark_node_type type);
+  cmark_node *cmark_node_new_with_mem(cmark_node_type type, cmark_mem *mem);
+  void cmark_node_free(cmark_node *node);
+  cmark_node *cmark_node_next(cmark_node *node);
+  mark_node *cmark_node_previous(cmark_node *node);
+  cmark_node *cmark_node_parent(cmark_node *node);
+  cmark_node *cmark_node_first_child(cmark_node *node);
+  cmark_node *cmark_node_last_child(cmark_node *node);
+
+
+  /* ## Iterator */
+
+  typedef enum {
+    CMARK_EVENT_NONE,
+    CMARK_EVENT_DONE,
+    CMARK_EVENT_ENTER,
+    CMARK_EVENT_EXIT
+  } cmark_event_type;
+
+
+  cmark_iter *cmark_iter_new(cmark_node *root);
+  void cmark_iter_free(cmark_iter *iter);
+  cmark_event_type cmark_iter_next(cmark_iter *iter);
+  cmark_node *cmark_iter_get_node(cmark_iter *iter);
+  cmark_event_type cmark_iter_get_event_type(cmark_iter *iter);
+  cmark_node *cmark_iter_get_root(cmark_iter *iter);
+  void cmark_iter_reset(cmark_iter *iter, cmark_node *current, cmark_event_type event_type);
+
+  /* ## Accessors */
+
+  void *cmark_node_get_user_data(cmark_node *node);
+  int cmark_node_set_user_data(cmark_node *node, void *user_data);
+  cmark_node_type cmark_node_get_type(cmark_node *node);
+  const char *cmark_node_get_type_string(cmark_node *node);
+  const char *cmark_node_get_literal(cmark_node *node);
+  int cmark_node_set_literal(cmark_node *node, const char *content);
+  int cmark_node_get_heading_level(cmark_node *node);
+  int cmark_node_set_heading_level(cmark_node *node, int level);
+  cmark_list_type cmark_node_get_list_type(cmark_node *node);
+  int cmark_node_set_list_type(cmark_node *node,cmark_list_type type);
+  cmark_delim_type cmark_node_get_list_delim(cmark_node *node);
+  int cmark_node_set_list_delim(cmark_node *node,cmark_delim_type delim);
+  int cmark_node_get_list_start(cmark_node *node);
+  int cmark_node_set_list_start(cmark_node *node, int start);
+  int cmark_node_get_list_tight(cmark_node *node);
+  int cmark_node_set_list_tight(cmark_node *node, int tight);
+  const char *cmark_node_get_fence_info(cmark_node *node);
+  int cmark_node_set_fence_info(cmark_node *node, const char *info);
+  const char *cmark_node_get_url(cmark_node *node);
+  int cmark_node_set_url(cmark_node *node, const char *url);
+  const char *cmark_node_get_title(cmark_node *node);
+  int cmark_node_set_title(cmark_node *node, const char *title);
+  const char *cmark_node_get_on_enter(cmark_node *node);
+  int cmark_node_set_on_enter(cmark_node *node,const char *on_enter);
+  const char *cmark_node_get_on_exit(cmark_node *node);
+  int cmark_node_set_on_exit(cmark_node *node, const char *on_exit);
+  int cmark_node_get_start_line(cmark_node *node);
+  int cmark_node_get_start_column(cmark_node *node);
+  int cmark_node_get_end_line(cmark_node *node);
+  int cmark_node_get_end_column(cmark_node *node);
+
+  /* ## Tree Manipulation */
+  void cmark_node_unlink(cmark_node *node);
+  int cmark_node_insert_before(cmark_node *node,cmark_node *sibling);
+  int cmark_node_insert_after(cmark_node *node, cmark_node *sibling);
+  int cmark_node_replace(cmark_node *oldnode, cmark_node *newnode);
+  int cmark_node_prepend_child(cmark_node *node, cmark_node *child);
+  int cmark_node_append_child(cmark_node *node, cmark_node *child);
+  void cmark_consolidate_text_nodes(cmark_node *root);
+
+  /* ## Parsing */
+  cmark_parser *cmark_parser_new(int options);
+  cmark_parser *cmark_parser_new_with_mem(int options, cmark_mem *mem);
+  void cmark_parser_free(cmark_parser *parser);
+  void cmark_parser_feed(cmark_parser *parser, const char *buffer, size_t len);
+  cmark_node *cmark_parser_finish(cmark_parser *parser);
+  cmark_node *cmark_parse_document(const char *buffer, size_t len, int options);
+  cmark_node *cmark_parse_file(FILE *f, int options);
+
+
+  /* ## Rendering */
+  char *cmark_render_xml(cmark_node *root, int options);
+  char *cmark_render_html(cmark_node *root, int options);
+  char *cmark_render_man(cmark_node *root, int options, int width);
+  char *cmark_render_commonmark(cmark_node *root, int options, int width);
+  char *cmark_render_latex(cmark_node *root, int options, int width);
+
+  /* ## Version information */
+  int cmark_version(void);
+  const char *cmark_version_string(void);
+
+  ]]
 
 local cmark = {}
 
@@ -106,71 +174,147 @@ local as_string = function(f)
    end
 end
 
+-- Simple Interface
+
+cmark.markdown_to_html      = c.cmark_markdown_to_html
+
+--[[
+-- Node Structure
+-- Error status
+cmark.NONE      = c.CMARK_NODE_NONE
+
+-- Block
 cmark.DOCUMENT      = c.CMARK_NODE_DOCUMENT
 cmark.BLOCK_QUOTE   = c.CMARK_NODE_BLOCK_QUOTE
 cmark.LIST          = c.CMARK_NODE_LIST
-cmark.LIST_ITEM     = c.CMARK_NODE_LIST_ITEM
+cmark.ITEM          = c.CMARK_NODE_ITEM
 cmark.CODE_BLOCK    = c.CMARK_NODE_CODE_BLOCK
-cmark.HTML          = c.CMARK_NODE_HTML
+cmark.HTML_BLOCK    = c.CMARK_NODE_HTML_BLOCK
+cmark.CUSTOM_BLOCK  = c.CMARK_NODE_CUSTOM_BLOCK
 cmark.PARAGRAPH     = c.CMARK_NODE_PARAGRAPH
-cmark.HEADER        = c.CMARK_NODE_HEADER
-cmark.HRULE         = c.CMARK_NODE_HRULE
-cmark.REFERENCE_DEF = c.CMARK_NODE_REFERENCE_DEF
+cmark.HEADING       = c.CMARK_NODE_HEADING
+cmark.THEMATIC_BREAK   = c.CMARK_NODE_THEMATIC_BREAK
+
+cmark.FIRST_BLOCK   = c.CMARK_NODE_FIRST_BLOCK
+cmark.LAST_BLOCK    = c.CMARK_NODE_LAST_BLOCK
+
+-- Inline
+
 cmark.TEXT          = c.CMARK_NODE_TEXT
 cmark.SOFTBREAK     = c.CMARK_NODE_SOFTBREAK
 cmark.LINEBREAK     = c.CMARK_NODE_LINEBREAK
-cmark.INLINE_CODE   = c.CMARK_NODE_INLINE_CODE
-cmark.INLINE_HTML   = c.CMARK_NODE_INLINE_HTML
+cmark.CODE          = c.CMARK_NODE_CODE
+cmark.HTML_INLINE   = c.CMARK_NODE_HTML_INLINE
+cmark.CUSTOM_INLINE = c.CMARK_NODE_CUSTOM_INLINE
 cmark.EMPH          = c.CMARK_NODE_EMPH
 cmark.STRONG        = c.CMARK_NODE_STRONG
-cmark.LINK          = c.CMARK_NODE_LINK
+cmark.HTML_LINK     = c.CMARK_NODE_LINK
 cmark.IMAGE         = c.CMARK_NODE_IMAGE
+cmark.EMPH          = c.CMARK_NODE_EMPH
+cmark.STRONG        = c.CMARK_NODE_STRONG
+
+cmark.FIRST_INLINE  = c.CMARK_NODE_FIRST_INLINE
+cmark.LAST_INLINE   = c.CMARK_NODE_LAST_INLINE
+
+--lists types
 cmark.NO_LIST       = c.CMARK_NO_LIST
 cmark.BULLET_LIST   = c.CMARK_BULLET_LIST
 cmark.ORDERED_LIST  = c.CMARK_ORDERED_LIST
-cmark.PERIOD_DELIM  = c.CMARK_PERIOD_DELIM
-cmark.PAREN_DELIM   = c.CMARK_PAREN_DELIM
-cmark.markdown_to_html = as_string(c.cmark_markdown_to_html)
+
+--delim type
+cmark.NO_DELIM       = c.CMARK_NO_DELIM
+cmark.PERIOD_DELIM   = c.CMARK_PERIOD_DELIM
+cmark.PAREN_DELIM    = c.CMARK_PAREN_DELIM
+
+-- Creating and Destroying Nodes
+-- cmark.markdown_to_html = as_string(c.cmark_markdown_to_html)
 cmark.node_new = c.cmark_node_new
+-- cmark_node *cmark_node_new_with_mem
 cmark.node_free = c.cmark_node_free
+
+-- Tree Traversal
 cmark.node_next = c.cmark_node_next
 cmark.node_previous = c.cmark_node_previous
 cmark.node_parent = c.cmark_node_parent
 cmark.node_first_child = c.cmark_node_first_child
 cmark.node_last_child = c.cmark_node_last_child
-cmark.node_get_type = c.cmark_node_get_type
-cmark.node_get_string_content =
-   as_string(c.cmark_node_get_string_content)
-cmark.node_set_string_content = c.cmark_node_set_string_content
-cmark.node_get_header_level = c.cmark_node_get_header_level
-cmark.node_set_header_level = c.cmark_node_set_header_level
-cmark.node_get_list_type = c.cmark_node_get_list_type
-cmark.node_set_list_type = c.cmark_node_set_list_type
-cmark.node_get_list_start = c.cmark_node_get_list_start
-cmark.node_set_list_start = c.cmark_node_set_list_start
-cmark.node_get_list_tight = c.cmark_node_get_list_tight
-cmark.node_set_list_tight = c.cmark_node_set_list_tight
-cmark.node_get_fence_info = as_string(c.cmark_node_get_fence_info)
-cmark.node_set_fence_info = c.cmark_node_set_fence_info
-cmark.node_get_url = as_string(c.cmark_node_get_url)
-cmark.node_set_url = c.cmark_node_set_url
-cmark.node_get_title = as_string(c.cmark_node_get_title)
-cmark.node_set_title = c.cmark_node_set_title
-cmark.node_get_start_line = c.cmark_node_get_start_line
-cmark.node_get_start_column = c.cmark_node_get_start_column
-cmark.node_get_end_line = c.cmark_node_get_end_line
-cmark.node_unlink = c.cmark_node_unlink
-cmark.node_insert_before = c.cmark_node_insert_before
-cmark.node_insert_after = c.cmark_node_insert_after
-cmark.node_prepend_child = c.cmark_node_prepend_child
-cmark.node_append_child = c.cmark_node_append_child
-cmark.parser_new = c.cmark_parser_new
-cmark.parser_free = c.cmark_parser_free
-cmark.parser_finish = c.cmark_parser_finish
-cmark.parser_feed = c.cmark_parser_feed
-cmark.parse_document = c.cmark_parse_document
-cmark.render_ast = as_string(c.cmark_render_ast)
-cmark.render_html = as_string(c.cmark_render_html)
+
+-- Iterator
+cmark.EVENT_NONE  = c.CMARK_EVENT_NONE
+cmark.EVENT_DONE  = c.CMARK_EVENT_DONE
+cmark.EVENT_ENTER = c.CMARK_EVENT_ENTER
+cmark.EVENT_EXIT  = c.CMARK_EVENT_EXIT
+
+cmark.iter_new  =  c.cmark_iter_new
+cmark.iter_free =  c.cmark_iter_free
+cmark.iter_next =  c.cmark_iter_next
+cmark.iter_get_node =  c.cmark_iter_get_node
+cmark.iter_get_event_type =  c.cmark_iter_get_event_type
+cmark.iter_get_root =  c.cmark_iter_get_root
+cmark.iter_reset =  c.cmark_iter_reset
+
+-- Accessors
+
+cmark.node_get_user_data =  c.cmark_node_get_user_data
+cmark.node_set_user_data =  c.cmark_node_set_user_data
+cmark.node_get_type =  c.cmark_node_get_type
+cmark.node_get_type_string =  c.cmark_node_get_type_string
+-- cmark.node_get_string_content =as_string(c.cmark_node_get_string_content)
+cmark.node_get_literal =  c.cmark_node_get_literal
+cmark.node_set_literal =  c.cmark_node_set_literal
+cmark.node_get_heading_level   =  c.cmark_node_get_heading_level
+cmark.node_set_heading_level   =  c.cmark_node_set_heading_level
+cmark.node_node_set_list_type  =  c.cmark_node_set_list_type
+cmark.node_node_get_list_delim =  c.cmark_node_get_list_delim
+cmark.node_set_list_delim      =  c.cmark_node_set_list_delim
+cmark.node_get_list_start      =  c.cmark_node_get_list_start
+cmark.node_set_list_start      =  c.cmark_node_set_list_start
+cmark.node_get_list_tight      =  c.cmark_node_get_list_tight
+cmark.node_get_fence_info      =  c.cmark_node_get_fence_info
+cmark.node_set_list_tight      =  c.cmark_node_set_list_tight
+cmark.node_get_url      =  c.cmark_node_get_url
+cmark.node_set_url      =  c.cmark_node_set_url
+cmark.node_set_title      =  c.cmark_node_set_title
+cmark.node_get_on_enter      =  c.cmark_node_get_on_enter
+cmark.node_get_on_exit      =  c.cmark_node_get_on_exit
+cmark.node_set_on_exit      =  c.cmark_node_set_on_exit
+cmark.node_get_start_line      =  c.cmark_node_get_start_line
+cmark.node_get_end_line      =  c.cmark_node_get_end_line
+cmark.node_get_start_column     =  c.cmark_node_get_start_column
+cmark.node_get_end_column     =  c.cmark_node_get_end_column
+
+-- Tree Manipulation
+cmark.node_unlink     =  c.cmark_node_unlink
+cmark.node_insert_before     =  c.cmark_node_insert_before
+cmark.node_insert_after     =  c.cmark_node_insert_after
+cmark.node_prepend_child     =  c.cmark_node_prepend_child
+cmark.node_append_child     =  c.cmark_node_append_child
+cmark.consolidate_text_nodes    =  c.cmark_consolidate_text_nodes
+
+-- Parsing
+-- * Simple interface:
+-- cmark.parse_document("Hello *world*", 13,CMARK_OPT_DEFAULT);
+cmark.parser_new     =  c.cmark_parser_new
+cmark.parser_new_with_mem     =  c.cmark_parser_new_with_mem
+cmark.parser_free     =  c.cmark_parser_free
+cmark.parser_new     =  c.cmark_parser_new
+cmark.parser_new     =  c.cmark_parser_new
+cmark.parser_feed     =  c.cmark_parser_feed
+cmark.parser_finish    =  c.cmark_parser_finish
+cmark.parser_feed     =  c.cmark_parse_document
+cmark.cparse_file     =  c.cmark_parse_file
+
+-- Rendering
+cmark.render_xml     =  c.cmark_render_xml
+cmark.render_html     =  c.cmark_render_html
+cmark.render_commonmark   =  c.cmark_render_commonmark
+cmark.render_latex     =  c.cmark_render_latex
+--]]
+-- Version information
+cmark.version        =  c.cmark_version
+cmark.version_string = c.cmark_version_string
+
+--[[
 
 local type_table = {
    'block_quote',
@@ -257,5 +401,5 @@ cmark.walk = function(cur)
       return direction, node
    end
 end
-
+--]]
 return cmark
